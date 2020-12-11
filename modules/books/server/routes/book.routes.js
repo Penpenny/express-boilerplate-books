883b1242a487ddd105a4ec1ffa04676c25213aef
Shraddha <75466832+pophaleshraddha@users.noreply.github.com>
2020-12-11 10:47:31 +0530

    Update book.routes.js

--- modules/books/server/routes/book.routes.js
+++ modules/books/server/routes/book.routes.js
@@ -1,11 +1,9 @@
 const { books } = require("../controllers/book.controller.js");
 module.exports = function(app) {
-    // User Routes
-    const users = require('../controllers');
 
     // Setting up the books api
     app.route('/create', controller.create);
     app.route('/read', controller.read);
     app.route('/update', controller.update);
-    app.route('/remove/:user', controller.remove);
+    app.route('/remove/:book', controller.remove);
 };
