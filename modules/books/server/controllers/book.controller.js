513adf9d1c45ad53af0aaf4b54fa3936a51b6898
Shraddha <75466832+pophaleshraddha@users.noreply.github.com>
2020-12-11 10:39:48 +0530

    Update book.controller.js

--- modules/books/server/controllers/book.controller.js
+++ modules/books/server/controllers/book.controller.js
@@ -1,90 +1,54 @@
-const db = require("../models");
-const Book = db.books;
+const db = require("./modules/books/models");
+const bookdb = db.books;
+const Key = mongoose.model('Key');
 
-// Create and Save a new Book
-exports.create = (req, res) => {
-  // Validate request
-  if (!req.body.title) {
-    res.status(400).send({ message: "Content can not be empty!" });
-    return;
+  // Save book in the database
+  async function create(book) {
+    try {
+        const key = new Key();
+        key.key = chance.guid();
+        key.book = book;
+        const result = await key.save(book);
+        return result.key;
+    } catch (e) {
+        throw new errors.InvalidData(e);
+    }
+}
+
+
+// Retrieve one books from the database.
+async function read(book) {
+  try {
+      const result = await Key.findOne({ book });
+      if (!result) throw new errors.InvalidData();
+      return result.key;
+  } catch (e) {
+      throw new errors.InvalidData(e);
   }
-
-  // Create a Book
-  const book = new Book({
-    title: req.body.title,
-    description: req.body.description,
-    year: req.body.year 
-  });
-
-  // Save Book in the database
-  book
-    .save(book)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Book."
-      });
-    });
-};
-
-// Retrieve all books from the database.
-exports.findAll = (req, res) => {
-  const title = req.query.title;
-  var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
-
-  Book.find(condition)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving books."
-      });
-    });
-};
-
-// Update a Book by the id in the request
-exports.update = (req, res) => {
-  if (!req.body) {
-    return res.status(400).send({
-      message: "Data to update can not be empty!"
-    });
+}
+// Update one books from the database.
+async function update(book) {
+  try {
+      const result = await Key.findOneAndUpdate({ book }, { $set: { key: chance.guid() } }, { new: true });
+      return result.key;
+  } catch (e) {
+      throw new errors.InvalidData(e);
   }
-
-  const id = req.params.id;
-
-  Book.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-    .then(data => {
-      if (!data) {
-        res.status(404).send({
-          message: `Cannot update Book with id=${id}. Maybe Book was not found!`
-        });
-      } else res.send({ message: "Book was updated successfully." });
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error updating Book with id=" + id
-      });
-    });
-};
+}
 
 // Delete all books from the database.
-exports.deleteAll = (req, res) => {
-  Book.deleteMany({})
-    .then(data => {
-      res.send({
-        message: `${data.deletedCount} books were deleted successfully!`
-      });
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while removing all books."
-      });
-    });
-};
+async function remove(book) {
+  try {
+      const result = await Key.findOneAndRemove({ book });
+      return result.key;
+  } catch (e) {
+      throw new errors.InvalidData(e);
+  }
+}
 
+module.exports = {
+  create,
+  read,
+  update,
+  remove,
+};
