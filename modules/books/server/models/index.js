ab0a92a00212008b1b79d276d904c5daa0d996d3
Shraddha <75466832+pophaleshraddha@users.noreply.github.com>
2020-12-14 15:50:02 +0530

    Delete index.js

--- modules/books/server/models/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-mongoose.connect(config.db.uri, options);
-const db = {};
-db.mongoose = mongoose;
-db.books = require("./book.model.js")(mongoose);
-
-module.exports = db;
